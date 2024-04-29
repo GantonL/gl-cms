e06d97fe344c3a55cf82e0930e203a810b55591b
GantonL <45093712+GantonL@users.noreply.github.com>
2024-04-29 17:55:39 +0300

    added firebase, update gitignore, change navitems to support current user wip

--- src/lib/components/navigation-menu/configuration.ts
+++ src/lib/components/navigation-menu/configuration.ts
@@ -1,9 +1,19 @@
 import type { NavigationLink } from "$lib/models/navigation-link";
-import { PanelsTopLeft, Settings } from "lucide-svelte";
+import type { UserRole } from "$lib/types/user-role";
+import { PanelsTopLeft, Settings, Users } from "lucide-svelte";
 
-export const NavigationItemsConfiguration: NavigationLink[] = [
+const AdminItemsConfiguration: NavigationLink[] = [
+  { label: 'Users', icon: Users, link: '/users' },    
+];
+
+const NavigationItemsConfiguration: NavigationLink[] = [
   { label: 'Overview', icon: PanelsTopLeft, link: '/' },
 ];
+
+export const ItemsConfiguration: Record<UserRole, NavigationLink[]> = {
+  admin: AdminItemsConfiguration,
+  user: NavigationItemsConfiguration,
+}
 
 export const MoreNavigationItems: NavigationLink[] = [
   { label: 'Settings', icon: Settings, link: '/settings' },
--- src/lib/components/navigation-menu/navigation-menu.svelte
+++ src/lib/components/navigation-menu/navigation-menu.svelte
@@ -2,30 +2,27 @@
 	import { ChevronsLeft, ChevronsRight } from "lucide-svelte";
   import Button from "../ui/button/button.svelte";
   import { Separator } from "../ui/separator";
-  import { NavigationItemsConfiguration, MoreNavigationItems } from "./configuration";
+  import { MoreNavigationItems } from "./configuration";
 	import NavigationItem from "./navigation-item.svelte";
+	import type { NavigationLink } from "$lib/models/navigation-link";
   export let currentPath: string;
-  export let expanded: boolean = true;
-
+  export let items: NavigationLink[] = [];
+  let expanded = true;
 </script>
 <nav class="flex items-start border-r flex-col h-full gap-2 p-2">
-  {#key expanded}
-    {#key currentPath}
-      {#each NavigationItemsConfiguration as navItem}
-        <NavigationItem navLink={navItem} active={navItem.link === currentPath} {expanded}/>
-      {/each}
-      <div class="flex-grow"></div>
-      <Button name="Toggle menu items labels" aria-label="Toggle menu items labels" variant="outline" class="w-full" on:click={() => expanded = !expanded} >
-        {#if expanded}
-          <ChevronsLeft size=16/>
-        {:else}
-          <ChevronsRight size=16/>
-        {/if}
-      </Button>
-      <Separator />
-      {#each MoreNavigationItems as navItem}
-        <NavigationItem navLink={navItem} active={navItem.link === currentPath} {expanded}/>
-      {/each}
-    {/key}
-  {/key}
+  {#each items as navItem}
+    <NavigationItem navLink={navItem} active={navItem.link === currentPath} {expanded}/>
+  {/each}
+  <div class="flex-grow"></div>
+  <Button name="Toggle menu items labels" aria-label="Toggle menu items labels" variant="outline" class="w-full" on:click={() => expanded = !expanded} >
+    {#if expanded}
+      <ChevronsLeft size=16/>
+    {:else}
+      <ChevronsRight size=16/>
+    {/if}
+  </Button>
+  <Separator />
+  {#each MoreNavigationItems as navItem}
+    <NavigationItem navLink={navItem} active={navItem.link === currentPath} {expanded}/>
+  {/each}
 </nav>
