b04fac965ec65d2b833e88ba565aa2943ac571fa
GantonL <45093712+GantonL@users.noreply.github.com>
2024-05-24 16:14:05 +0300

    default project navigation

--- src/routes/(user)/projects/[slug]/+layout.svelte
+++ src/routes/(user)/projects/[slug]/+layout.svelte
@@ -5,6 +5,20 @@
   import * as ToggleGroup from "$lib/components/ui/toggle-group";
 	import { ProjectNavigationConfigurations } from "./configurations";
 	import type { Project } from "$lib/models/project";
+	import { afterNavigate, goto } from "$app/navigation";
+  
+  let currentNavItem = '';
+  afterNavigate((navigation) => {
+    const pathNameSegments = navigation?.to?.url?.pathname?.split('/');
+    if (!pathNameSegments?.length) { return; }
+    const last = pathNameSegments[pathNameSegments.length - 1];
+    if (last && last !== navigation?.to?.params?.slug) {
+      currentNavItem = last;
+    }
+    if (!currentNavItem) {
+      goto(`/projects/${project.id}/${ProjectNavigationConfigurations[project.type][0].value}`);
+    }
+  });
 
   onMount(() => {
     currentProject.set(project.name);
@@ -15,7 +29,7 @@
   $: project = $page.data.project as Project;
 </script>
 
-<ToggleGroup.Root type="single" class="border rounded-md">
+<ToggleGroup.Root type="single" class="border rounded-md" bind:value={currentNavItem}>
   {#each ProjectNavigationConfigurations[project.type] as navMenu}
     <ToggleGroup.Item value={navMenu.value}>
       <a href={`/projects/${project.id}/${navMenu.value}`}>{navMenu.name}</a>
