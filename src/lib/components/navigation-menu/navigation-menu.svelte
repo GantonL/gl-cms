2d1380bccb1ec098025b00b56ef6d4eda1bf6f63
GantonL <45093712+GantonL@users.noreply.github.com>
2024-06-17 20:29:41 +0300

    active link support for project menu items

--- src/lib/components/navigation-menu/navigation-menu.svelte
+++ src/lib/components/navigation-menu/navigation-menu.svelte
@@ -18,7 +18,7 @@
 <nav class="flex items-start border-r flex-col h-full gap-2 p-2">
   {#if items.length }
     {#each items as navItem}
-      <NavigationItem navLink={navItem} active={navItem.link === currentPath} {expanded}/>
+      <NavigationItem navLink={navItem} active={navItem.link === currentPath || currentPath.includes(`/${navItem.link}`)} {expanded}/>
     {/each}
     <div class="flex-grow"></div>
     <Button name="Toggle menu items labels" aria-label="Toggle menu items labels" variant="outline" class="w-full" on:click={() => expanded = !expanded} >
