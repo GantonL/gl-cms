f3b20ef748e8901d02f4cf5e641d483947984c2f
GantonL <45093712+GantonL@users.noreply.github.com>
2024-06-23 20:51:02 +0300

    tooltip and default collaped navgation menu

--- src/lib/components/navigation-menu/navigation-item.svelte
+++ src/lib/components/navigation-menu/navigation-item.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
 	import type { NavigationLink } from "$lib/models/navigation-link";
+	import * as Tooltip from "../ui/tooltip";
 
   export let navLink: NavigationLink;
   export let active: boolean;
   export let expanded: boolean;
 
 </script>
-<a href={navLink.link} class="flex flex-row items-center gap-2 rounded-md px-4 py-2 hover:bg-secondary w-full" class:bg-primary-foreground={!active} class:bg-secondary={active}>
-  {#if navLink.icon}
-    <svelte:component this={navLink.icon} size=16/>
-  {/if}
-  <span class:hidden={!expanded} class="leading-none">{navLink.label}</span>
-</a>
+<Tooltip.Root>
+  <Tooltip.Trigger class="w-full">
+    <a href={navLink.link} class="flex flex-row items-center gap-2 rounded-md px-4 py-2 hover:bg-secondary w-full" class:bg-primary-foreground={!active} class:bg-secondary={active}>
+      {#if navLink.icon}
+        <svelte:component this={navLink.icon} size=16/>
+      {/if}
+      <span class:hidden={!expanded} class="leading-none">{navLink.label}</span>
+    </a>
+  </Tooltip.Trigger>
+  <Tooltip.Content>
+    {#if !expanded}
+      {navLink.label}
+    {/if}
+  </Tooltip.Content>
+</Tooltip.Root>
--- src/lib/components/navigation-menu/navigation-menu.svelte
+++ src/lib/components/navigation-menu/navigation-menu.svelte
@@ -13,13 +13,23 @@
     ...ItemsConfiguration[$user.role],
     ...projectItemsConfiguraion[$currentProject?.type ?? ProjectType.None]
     ] : [];
-  let expanded = true;
+  let expanded = false;
 </script>
 <nav class="flex items-start border-r flex-col h-full gap-2 p-2">
   {#if items.length }
     {#each items as navItem}
       <NavigationItem navLink={navItem} active={navItem.link === currentPath || currentPath.includes(`/${navItem.link}`)} {expanded}/>
     {/each}
+    {#each MoreNavigationItems as navItem}
+    <Separator />
+    <NavigationItem navLink={navItem} active={navItem.link === currentPath} {expanded}/>
+    {/each}
+    {:else }
+    <div class="border rounded-md bg-muted text-muted-foreground flex flex-col items-center justify-center gap-2 p-2 w-full">
+      <HeartCrack />
+      <span class="prose prose-sm text-center max-w-24 text-muted-foreground">Menu is not available</span>
+    </div>
+    {/if}
     <div class="flex-grow"></div>
     <Button name="Toggle menu items labels" aria-label="Toggle menu items labels" variant="outline" class="w-full" on:click={() => expanded = !expanded} >
       {#if expanded}
@@ -28,14 +38,4 @@
         <ChevronsRight size=16/>
       {/if}
     </Button>
-    {#each MoreNavigationItems as navItem}
-      <Separator />
-      <NavigationItem navLink={navItem} active={navItem.link === currentPath} {expanded}/>
-    {/each}
-  {:else }
-    <div class="border rounded-md bg-muted text-muted-foreground flex flex-col items-center justify-center gap-2 p-2 w-full">
-      <HeartCrack />
-      <span class="prose prose-sm text-center max-w-24 text-muted-foreground">Menu is not available</span>
-    </div>
-  {/if}
-</nav>
+  </nav>
