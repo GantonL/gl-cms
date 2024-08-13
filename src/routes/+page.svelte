282f4e3e0ef6c374303b0ae36296ce4efbe0fe6e
GantonL <45093712+GantonL@users.noreply.github.com>
2024-08-13 16:21:14 +0300

    change welcome page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,26 @@
 <script>
 	import { user } from "$lib/client/stores";
   import { Button } from "$lib/components/ui/button";
+	import Separator from "$lib/components/ui/separator/separator.svelte";
+	import { Presentation, ShieldCheck } from "lucide-svelte";
 </script>
-<h1 class="text-2xl">Welcome to GL CMS</h1>
-<p class="text-center text-lg">A unique content management system for your projects</p>
+<h1 class="text-2xl">Welcome to GL projects</h1>
+<p class="text-center text-lg">A unique management system for your GL projects</p>
+<p class="text-center text-lg">Manage everything related to your projects from content to clients</p>
 {#if !$user}
-  <a href="/login">
-    <Button variant="default" size="lg">Login</Button>
-  </a>
-{/if}
+  <div class="flex flex-row gap-4 p-4">
+    <a href="/login">
+      <Button variant="default" size="lg"  class="flex flex-row gap-2 items-center">
+        <ShieldCheck size=16/>
+        <span>Login</span>
+      </Button>
+    </a>
+    <Separator orientation="vertical"/>
+    <a href="/demo-request">
+      <Button variant="secondary" size="lg"  class="flex flex-row gap-2 items-center">
+        <Presentation size=16/>
+        <span>request a demo</span>
+      </Button>    
+    </a>
+  </div>
+{/if} 
