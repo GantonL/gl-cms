b4e58098cb5930868b6e9a340b89df425e5ddbb5
GantonL <45093712+GantonL@users.noreply.github.com>
2024-09-09 07:30:52 +0300

    project layout translation

--- src/routes/(user)/projects/[slug]/+layout.server.ts
+++ src/routes/(user)/projects/[slug]/+layout.server.ts
@@ -3,25 +3,26 @@ import { error } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
 import { getProject } from "$lib/server/projects.db";
 import { getUser } from "$lib/server/users.db";
+import { t } from "$lib/i18n/translations";
 
 export const load: LayoutServerLoad = async (event) => {
   const autheticatedUser = await getAuthenticatedUser(event);
   if (!autheticatedUser) {
-    error(401, 'Unauthorized');
+    error(401, t.get('common.unauthorized'));
   }
   const isAdmin = await isAdminUser(autheticatedUser.uid);
   const user = await getUser(autheticatedUser.email!);
   const project = await getProject(event.params.slug);
   if (!project) {
-    error(404, 'Unknown project');
+    error(404, t.get('common.unknown_project'));
   }
   if (!isAdmin && !user?.projects?.includes(project!.name)) {
-    error(403, 'Access denied, you are not assigned to this project');
+    error(403, t.get('common.not_assigned_to_project_error'));
   }
   return {
     project,
     seo: {
-      title: project?.name || 'Unknown project',
+      title: project?.name || t.get('common.unknown_project'),
     }
   }
 }
--- src/routes/(user)/projects/[slug]/+page.svelte
+++ src/routes/(user)/projects/[slug]/+page.svelte
@@ -1,1 +1,6 @@
-<h1>Dashboard</h1>
+<script>
+
+	import { t } from "$lib/i18n/translations";
+
+</script>
+<h1>{$t('common.dashboard')}</h1>
