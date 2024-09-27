d1a58d83e4a8a1691b69a19fb6433c6e87278559
GantonL <45093712+GantonL@users.noreply.github.com>
2024-09-27 19:00:45 +0300

    minor styling change

--- src/routes/(user)/projects/[slug]/+page.svelte
+++ src/routes/(user)/projects/[slug]/+page.svelte
@@ -4,28 +4,29 @@
 	import * as Card from "$lib/components/ui/card";
 	import { t } from "$lib/i18n/translations";
 	import type { Project } from "$lib/models/project";
-	import { Coins, LoaderCircle } from "lucide-svelte";
+	import { HandCoins, LoaderCircle } from "lucide-svelte";
 	import { onMount } from "svelte";
-
+	
 	let totalPaymentsInProgress = true;
 	let totalPayments = 0;
 
 	$: project = $page.data.project as Project;
-
+	
 	onMount(() => {
 		initializeData();
 	})
-	
+
+
 	function initializeData() {
 		getTotalPayments();
 	}
-
+	
 	function getTotalPayments() {
 		totalPaymentsInProgress = true;
 		fetch(`/projects/${project.id}/dashboard/total-payments`, { method: 'GET' })
 			.then((res) => {
-				res.json()?.then((total) => {
-					totalPayments = total;
+				res.json()?.then((data) => {
+					totalPayments = data;
 				})
 				.finally(() => totalPaymentsInProgress = false)
 			})
@@ -38,7 +39,7 @@
 	<Card.Root>
 		<Card.Header>
 			<div class="flex flex-row gap-4 items-start">
-				<Coins size=32/>
+				<HandCoins size=32/>
 				<div class="flex flex-col gap-2">
 					<Card.Title>{$t('common.total_payments')}</Card.Title>
 					<Card.Description>{$t('common.total_payments_dashboard_description')}</Card.Description>
@@ -50,9 +51,7 @@
 				<LoaderCircle size=18 class="animate-spin"/>
 			{:else}
 				<span class="font-bold text-lg text-primary">
-					<Currency>
-						{totalPayments}
-					</Currency>
+					<Currency styleClass="w-fit">{totalPayments}</Currency>
 				</span>
 			{/if}
 		</Card.Content>
