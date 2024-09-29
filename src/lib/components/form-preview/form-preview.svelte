0f8147ba62d141de281ea8f47315300429661a24
GantonL <45093712+GantonL@users.noreply.github.com>
2024-09-29 20:19:58 +0300

    added form shadow for form templates preview component

--- src/lib/components/form-preview/form-preview.svelte
+++ src/lib/components/form-preview/form-preview.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { t } from "$lib/i18n/translations";
-	import { Circle, CircleCheckBig } from "lucide-svelte";
+	import { Circle, CircleCheckBig, FileSignature } from "lucide-svelte";
 	import { Button } from "../ui/button";
 	import * as Card from "../ui/card";
 	import type { FormType } from "$lib/enums/form-type";
@@ -17,12 +17,26 @@
     dispath('checked', check);
   }
 </script>
-<Card.Root>
+<Card.Root class=" max-w-96">
   <Card.Header>
-    <div class="flex flex-row justify-between gap-4">
-      <div class="flex flex-col gap-2">
-        <Card.Title>{$t(`common.forms_types.${type}.title`)}</Card.Title>
-        <Card.Description>{$t(`common.forms_types.${type}.description`)}</Card.Description>
+    <div class="flex flex-row justify-between items-start gap-4">
+      <div class="flex flex-row gap-4">
+        <div class="border rounded-md h-24 w-20 flex flex-col items-center gap-2 p-2">
+          <div class="bg-secondary h-2 w-3/4 rounded-md"></div>
+          <div class="flex flex-col items-start gap-2 w-full">
+            <div class="bg-secondary border-b-2 w-full"></div>
+            <div class="bg-secondary border-b-2 w-3/4"></div>
+            <div class="bg-secondary border-b-2 w-1/3"></div>
+            <div class="bg-secondary border-b-2 w-1/2"></div>
+          </div>
+          <div class="flex items-end justify-end flex-grow w-full">
+            <FileSignature size=14 class=" text-secondary"/>
+          </div>
+        </div>
+        <div class="flex flex-col gap-2">
+          <Card.Title>{$t(`common.forms_types.${type}.title`)}</Card.Title>
+          <Card.Description>{$t(`common.forms_types.${type}.description`)}</Card.Description>
+        </div>
       </div>
       <Tooltip.Root>
         <Tooltip.Trigger>
@@ -37,14 +51,11 @@
         <Tooltip.Content>
           {#if check}
             {$t('common.remove_from_project')}
-            {:else}
+          {:else}
             {$t('common.add_to_project')}
           {/if}
         </Tooltip.Content>
       </Tooltip.Root>
     </div>
   </Card.Header>
-  <Card.Content>
-    <!-- Preview goes here -->
-  </Card.Content>
 </Card.Root>
