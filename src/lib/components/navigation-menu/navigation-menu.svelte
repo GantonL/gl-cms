6c12bb3c0608ef94495be6149644c34c2ea6f1a7
GantonL <45093712+GantonL@users.noreply.github.com>
2024-10-02 11:01:56 +0300

    hide menu when users are logedout

--- src/lib/components/navigation-menu/navigation-menu.svelte
+++ src/lib/components/navigation-menu/navigation-menu.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-	import { ChevronsLeft, ChevronsRight, HeartCrack } from "lucide-svelte";
+	import { ChevronsLeft, ChevronsRight } from "lucide-svelte";
   import Button from "../ui/button/button.svelte";
   import { ItemsConfiguration, projectItemsConfiguration } from "./configuration";
 	import NavigationItem from "./navigation-item.svelte";
 	import { currentProject, direction, user } from "$lib/client/stores";
 	import { ProjectType } from "$lib/enums/projects";
 	import type { NavigationLink } from "$lib/models/navigation-link";
-	import { t } from "$lib/i18n/translations";
 	import { onMount } from "svelte";
 
   export let currentPath: string;
@@ -38,23 +37,18 @@
     localStorage.setItem(navigationMenuExpansionStorageKey, String(expanded));
   }
 </script>
-<nav class="flex items-start border-r flex-col h-full gap-2 p-2">
-  {#if items.length > 0 }
+{#if items.length > 0 }
+  <nav class="flex items-start border-r flex-col h-full gap-2 p-2">
     {#each items as navItem}
       <NavigationItem navLink={navItem} active={navItem.link === currentPath || currentPath.includes(navItem.link)} {expanded}/>
     {/each}
-    {:else }
-    <div class="border rounded-md bg-muted text-muted-foreground flex flex-col items-center justify-center gap-2 p-2 w-full">
-      <HeartCrack />
-      <span class="prose prose-sm text-center max-w-24 text-muted-foreground">{$t('common.menu_not_available')}</span>
-    </div>
-    {/if}
     <div class="flex-grow"></div>
     <Button name="Toggle menu items labels" aria-label="Toggle menu items labels" variant="outline" class="w-full {$direction === 'rtl' ? 'rotate-180' : ''}" on:click={toggleNavigationMenu} >
       {#if expanded}
-        <ChevronsLeft size=16/>
+      <ChevronsLeft size=16/>
       {:else}
-        <ChevronsRight size=16/>
+      <ChevronsRight size=16/>
       {/if}
     </Button>
   </nav>
+{/if}
