2a0f5b49e1f5ac06d28acfa8d1b3d20f7a86d540
GantonL <45093712+GantonL@users.noreply.github.com>
2024-10-04 17:05:46 +0300

    signature events

--- src/lib/components/forms/markdowns/he/botox-agreement.md
+++ src/lib/components/forms/markdowns/he/botox-agreement.md
@@ -3,11 +3,11 @@ title: ''
 description: ''
 date: ''
 ---
-<script>
-  import FormShadowTemplate from '$lib/components/forms/form-shadow-template/form-shadow-template.svelte'
-  import SignaturePad from '$lib/components/signature-pad/signature-pad.svelte'
+<script lang="ts">
+  import FormShadowTemplate from '$lib/components/forms/form-shadow-template/form-shadow-template.svelte';
+  import SignaturePad from '$lib/components/signature-pad/signature-pad.svelte';
 </script>
 <FormShadowTemplate />
 <div class="h-72">
-  <SignaturePad />
+  <SignaturePad on:confirmed={(event) => console.log(event.detail)}/>
 </div>
--- src/lib/components/signature-pad/signature-pad.svelte
+++ src/lib/components/signature-pad/signature-pad.svelte
@@ -1,20 +1,26 @@
 <script lang="ts">
 	import SignaturePad from "signature_pad";
-	import { createEventDispatcher, onMount } from "svelte";
+	import { createEventDispatcher, onDestroy, onMount } from "svelte";
 	import { Button } from "../ui/button";
 	import { t } from "$lib/i18n/translations";
 
   let canvas: HTMLCanvasElement;
   let signature: SignaturePad;
+  let signatureExists = false;
   const dispath = createEventDispatcher();
 
   onMount(() => {
     signature = new SignaturePad(canvas, {
       backgroundColor: `rgb(255, 255, 255)`,
     });
+    signature.addEventListener("endStroke", endSignatureStorkeHander);
     window.onresize = resizeCanvas;
     resizeCanvas();
   });
+
+  function endSignatureStorkeHander() {
+    signatureExists = true;
+  }
 
   function resizeCanvas() {
     const ratio =  Math.max(window.devicePixelRatio || 1, 1);
@@ -25,21 +31,27 @@
 
   function onClear() {
     signature.clear();
+    signatureExists = false;
     return false;
   }
-
+  
   function onConfirm() {
-    dispath('confirmed', signature.toDataURL('image/svg+xml', {includeBackgroundColor: true}))
+    dispath('confirmed', signature.toDataURL('image/svg+xml', {includeBackgroundColor: true}));
   }
 
+  onDestroy(() => {
+    signature.removeEventListener('endStroke', endSignatureStorkeHander);
+    signature.off();
+  })
+
 </script>
-<div class="flex flex-col items-center w-full h-full">
+<div class="flex flex-col items-center w-full h-64">
   <div class="flex items-center justify-center p-2 border rounded-t-md w-full"><span>{$t('common.signature')}</span></div>
-  <div class="relative w-full flex-grow">
+  <div class="relative w-full h-64">
     <canvas bind:this={canvas} class="absolute left-0 top-0 w-full h-full border rounded-b-md bg-white"></canvas>
   </div>
   <div class="flex flex-row gap-2 items-center w-full py-2">
-    <Button class="flex-grow" on:click={onConfirm}>{$t('common.confirm')}</Button>
+    <Button class="flex-grow" disabled={!signatureExists} on:click={onConfirm}>{$t('common.confirm')}</Button>
     <Button class="flex-grow" variant="outline" on:click={onClear}>{$t('common.clear')}</Button>
   </div>
 </div>
